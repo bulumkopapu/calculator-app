610e1d945a4bd44af952a0624c766e06dd764006
Bulumko <bulumkopapu@gmail.com>
2021-11-30 14:13:58 +0200

    v1.2 Calculator App

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+const radioButtons = document.querySelectorAll('input[name="toggle-state"]');
+const body = document.body;
+const toggleBall = document.querySelector('.checkmark');
+const hOne = document.querySelector('h1');
+const paragraphs = document.querySelector('p');
+const themeNumbers = document.querySelectorAll('.theme-numbers p');
 
+
+
+const mainDisplay = document.querySelector('.display-section');
+const mainDisplayParagraph = document.querySelector('.display-section p');
+const calculatorArea = document.querySelector('.calculator');
+
+//EVENT LISTENER FOR RADIO BUTTONS
+for (const radioButton of radioButtons) {
+        radioButton.onclick = function(event) {
+            let eValue = event.target.value;
+
+        if(eValue == 1){
+            
+
+        } else if(eValue == 2){
+            
+
+        } else if(eValue == 3){
+           
+
+        }
+    }
+}
