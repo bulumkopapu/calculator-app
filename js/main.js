602ce8adccbe597e04e018583991aef2eb1e886e
Bulumko <bulumkopapu@gmail.com>
2021-11-30 15:02:09 +0200

    v1.3 Calculator App

--- js/main.js
+++ js/main.js
@@ -1,15 +1,44 @@
+const firstRadioButton = document.getElementById('one');
+const secondRadioButton = document.getElementById('two');
+const thirdRadioButton = document.getElementById('three');
+const cachedTheme = localStorage.getItem('theme');
+
 const radioButtons = document.querySelectorAll('input[name="toggle-state"]');
 const body = document.body;
-const toggleBall = document.querySelector('.checkmark');
-const hOne = document.querySelector('h1');
-const paragraphs = document.querySelector('p');
-const themeNumbers = document.querySelectorAll('.theme-numbers p');
+
+if(firstRadioButton.checked){
+    body.classList.add("theme-one");
+}
+
+if(cachedTheme){
+    body.className = "";
+    body.classList.add(cachedTheme);
+}
 
 
+for (const radioButton of radioButtons) {
+    radioButton.onclick = function(event) {
+        let eValue = event.target.value;
 
-const mainDisplay = document.querySelector('.display-section');
-const mainDisplayParagraph = document.querySelector('.display-section p');
-const calculatorArea = document.querySelector('.calculator');
+    if(eValue == 1){
+        body.className = "";
+        body.classList.add("theme-one");
+        window.localStorage.setItem('theme', 'theme-one');
+
+    } else if(eValue == 2){
+        body.className = "";
+        body.classList.add("theme-two");
+        window.localStorage.setItem('theme', 'theme-two');
+
+    } else if(eValue == 3){
+        body.className = "";
+        body.classList.add("theme-three");
+        window.localStorage.setItem('theme', 'theme-three');
+    }
+}
+}
+
+
 
 //EVENT LISTENER FOR RADIO BUTTONS
 for (const radioButton of radioButtons) {
@@ -17,13 +46,16 @@ for (const radioButton of radioButtons) {
             let eValue = event.target.value;
 
         if(eValue == 1){
-            
+            body.className = "";
+            body.classList.add("theme-one");
 
         } else if(eValue == 2){
-            
+            body.className = "";
+            body.classList.add("theme-two");
 
         } else if(eValue == 3){
-           
+            body.className = "";
+            body.classList.add("theme-three");
 
         }
     }
